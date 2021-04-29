060ee55900821406011436ad627b5709cee58dea
menezessn <mar_chelo10@hotmail.com>
2021-04-29 20:20:41 -0300

    Funções arrays

--- 09_funcoes_arrays.php
+++ 09_funcoes_arrays.php
@@ -11,5 +11,18 @@ array_values($array) = retorna um array com os valores do array
 
 array_merge ($array1, $array2) = retorna um array com os valores dos dois arrays
 
-$array_pop = exclui e retorna a ultima posição do array
+$array_pop($array) = exclui e retorna a ultima posição do array
 
+array_shift($array)= exclui e retorna a primeira posição do array
+
+array_unshift($array, "valor") = adiciona um ou mais elementos no inicio do array 
+
+array_push($array, "valor") = adiciona um ou mais elementos no final do array 
+
+array_combine($keys, $values) = mescla dois arrays, com o primeiro sendo os indices e o segundo os valores
+
+array_sum($soma)= calcula e retorna a soma dos elementos de uma array
+
+explode("/", "20/01/2001") = transforma uma string em uma array, o primeiro parametro é um delimitador e o segundo a string
+
+//implode("," , $array) = transforma uma array em string, o primeiro parametro é um delimitador e o segundo a string
